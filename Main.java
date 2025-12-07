873c3d1a624ab056a7a2107201138294f7988371
Andra <alghozali2123@gmail.com>
2025-12-07 17:09:12 +0700

    tambah main

--- Main.java
+++ Main.java
@@ -2,14 +2,6 @@ import view.ViewLogistik;
 
 public class Main {
     public static void main(String[] args) {
-        // ViewLogistik.main(args);
-        try {
-            var conn = DatabaseConnection.getConnection();
-            System.out.println("✅ Koneksi database SUKSES!");
-            conn.close();
-        } catch (Exception e) {
-            System.err.println("❌ Gagal koneksi:");
-            e.printStackTrace();
-        }
+         ViewLogistik.main(args);
     }
 }
