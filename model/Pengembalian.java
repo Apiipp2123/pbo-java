c469d4fb8d18f7ff67b1e23ec6f102fc7e4cbecd
Andra <alghozali2123@gmail.com>
2025-12-07 16:00:57 +0700

    tambah pengembalian

---
+++ model/Pengembalian.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Pengembalian {
+    private int id;
+    private double denda;
+    private LocalDate tgl_pengembalian;
+
+    public Pengembalian(int id, double denda, LocalDate tgl_pengembalian) {
+        this.id = id;
+        this.denda = denda;
+        this.tgl_pengembalian = tgl_pengembalian;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getDenda() {
+        return denda;
+    }
+
+    public void setDenda(double denda) {
+        this.denda = denda;
+    }
+
+    public LocalDate getTgl_pengembalian() {
+        return tgl_pengembalian;
+    }
+
+    public void setTgl_pengembalian(LocalDate tgl_pengembalian) {
+        this.tgl_pengembalian = tgl_pengembalian;
+    }
+
+    
+}
