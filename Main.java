c5bfa477ac05367d975cd5571c3d0312501d4190
Andra <alghozali2123@gmail.com>
2025-12-07 17:06:36 +0700

    tambah database koneksi

--- Main.java
+++ Main.java
@@ -2,6 +2,14 @@ import view.ViewLogistik;
 
 public class Main {
     public static void main(String[] args) {
-        ViewLogistik.main(args);
+        // ViewLogistik.main(args);
+        try {
+            var conn = DatabaseConnection.getConnection();
+            System.out.println("✅ Koneksi database SUKSES!");
+            conn.close();
+        } catch (Exception e) {
+            System.err.println("❌ Gagal koneksi:");
+            e.printStackTrace();
+        }
     }
 }
--- model/Pengembalian.java
+++ model/Pengembalian.java
@@ -6,11 +6,15 @@ public class Pengembalian {
     private int id;
     private double denda;
     private LocalDate tgl_pengembalian;
+    private Kendaraan kendaraan;
+    private Pelanggan pelanggan;
 
-    public Pengembalian(int id, double denda, LocalDate tgl_pengembalian) {
+    public Pengembalian(int id, double denda, LocalDate tgl_pengembalian, Kendaraan kendaraan, Pelanggan pelanggan) {
         this.id = id;
         this.denda = denda;
         this.tgl_pengembalian = tgl_pengembalian;
+        this.kendaraan = kendaraan;
+        this.pelanggan = pelanggan;
     }
 
     public int getId() {
@@ -35,6 +39,22 @@ public class Pengembalian {
 
     public void setTgl_pengembalian(LocalDate tgl_pengembalian) {
         this.tgl_pengembalian = tgl_pengembalian;
+    }
+
+    public Kendaraan getKendaraan() {
+        return kendaraan;
+    }
+
+    public void setKendaraan(Kendaraan kendaraan) {
+        this.kendaraan = kendaraan;
+    }
+
+    public Pelanggan getPelanggan() {
+        return pelanggan;
+    }
+
+    public void setPelanggan(Pelanggan pelanggan) {
+        this.pelanggan = pelanggan;
     }
 
     
